93900b4d73533d33b788e50341e19f53ca03e8bf
Miriam Idstein <idsteinmiriam@gmail.com>
2019-05-06 22:16:09 -0400

    Add project files.

---
+++ 5-6-19candidatetracker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _5_6_19candidatetracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
